e36668e2e0fa9da120f609714f664a3e22ac9f0f
mariutto5-debug <mariutto5@gmail.com>
2026-08-10 16:25:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const crypto = require('crypto');
 const http = require('http');
 
 const PORT = process.env.PORT || 8080;
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('OK');
+});
 const wss = new WebSocketServer({ server });
 
 // Pre-shared key for HMAC authentication
